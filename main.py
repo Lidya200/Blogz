591ae90ff41f56e29479e11589b7b28674f439df
Lidya200 <lidya.habte@gmail.com>
2017-07-20 16:08:51 -0700

    finshed working on bolgz

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from flask import Flask, request, redirect, render_template,session
+#__author__: "lidya habteselassie"
+
+# date: 07/20/2017
+# assignment: Blogz
+# collaborated with "Edan Aygoda"
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +11,8 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:12345@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 class Blog(db.Model):
 
@@ -40,13 +47,13 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             session['username']=username
-
-            return redirect('/')
+            return redirect('/newpost')
         else:
 
-            return '<h1>Invalid input, please try again!</h1>'
+            return render_template('/login.html')
 
     return render_template('login.html')
+
 @app.before_request
 def require_login():
     allowd_routes =['index', 'blog', 'login', 'signup']
@@ -97,6 +104,25 @@ def logout():
 
 @app.route("/blog", methods=["POST", "GET"])
 def blog():
+
+
+    blog_id = request.args.get('id')
+    blog_user = request.args.get('username')
+
+    if blog_id:
+        blog = Blog.query.filter_by(id=blog_id).first()
+        return render_template('blog.html', blog= blog, blog_id=blog_id)
+
+    elif blog_user:
+        user_id = User.query.filter_by(username=blog_user).first().id
+        posts = Blog.query.filter_by(owner_id=user_id).all()
+        return render_template('blog.html', posts=posts)
+
+
+    else:
+        blogs= Blog.query.all()
+        return render_template('blog.html', page_name = "blog users!", blogs=blogs)
+
     
     posts = Blog.query.all()
     return render_template('blog.html', posts= posts)
@@ -107,57 +133,13 @@ def blog():
     # owner = User.query.filter_by(username=session['username']).first()
 def index():
 
-    owner = User.query.filter_by(username=session['email']).first()
-
-    if request.method == 'POST':
-        error_title = ""
-        error_content =""
-        blog_title = request.form['title']
-        blog_content = request.form['content']
-        if len(blog_title) >=120 or len(blog_title) ==0:
-            error_title = "Your title length exceeds the limit, please shorten your title."
-            blog_title =""
-        if len(blog_content) >=1000 or len(blog_content) ==0:
-            error_content = "your content length exceeds the limit, please shorten your content."
-            blog_content = ""
-        if len(error_title)>0 or len(error_content)>0:
-            return render_template('index.html', title= blog_title, content= blog_content, error_title=error_title, error_content=error_content)
-        else:
-            new_blog = Blog(blog_title, blog_content, owner)
-            db.session.add(new_blog)
-            db.session.commit()
-            return redirect("/blog_page?id=" +str(new_blog.id))
-
-    # posts = Blog.query.filter_by(owner=username).all()
-
-    posts = Blog.query.all()
-
-    return render_template('index.html', posts= posts)
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
     #return render_template('index.html')
 
 @app.route('/newpost', methods=['post', 'get'])
 def add():
-    
-    blog_id = request.args.get('id')
-    blog_user = request.args.get('username')
-
-    if blog_id:
-        post = Blog.query.filter_by(id=blog_user).first()
-        return render_template('single_blog_page.html', page_name="Build a Blog", post= post)
-
-    elif blog_user:
-        user_id = User.query.filter_by(username=blog_user).first().id
-        posts = Blog.query.filter_by(owner_id=user_id).all()
-        return render_template('blog_page.html', posts=posts)
-
-
-    else:
-        users= User.query.all()
-        return render_template('user_name_list.html', page_name = "blog users!", users=users)
-
-
-
     post = ""
     new_blog = ""
     title_error = ""
@@ -165,6 +147,7 @@ def add():
     if request.method == "POST":
         #blog_id = request.form['id']
         blog_title = request.form['title']
+        owner = User.query.filter_by(username=session['username']).first()
         
         if not blog_title:
             title_error = "Please type a title for your blog post"
@@ -179,13 +162,14 @@ def add():
             elif len(blog_content)>1000:
                 content_error= "Your blog body is more than 1000 words."
             else:
-                new_blog = Blog(blog_title, blog_content,owner)
+
+                new_blog = Blog(blog_title, blog_content, owner)
                 db.session.add(new_blog)
                 db.session.commit()
 
                 blog_id = new_blog.id
        
-                return redirect("/singlepost?id=" + str(blog_id))
+                return redirect("/blog?id=" + str(blog_id))
 
     return render_template("newpost.html", post=new_blog, title_error=title_error,content_error=content_error)
 
